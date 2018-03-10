56ebdb41e37168fbfbad03b7603470c0dae5fc23
HootOwl17 <davidscout2010@hotmail.com>
2018-03-10 10:11:13 -0600

    Probando Arduino

--- mar10a_arduino/mar10a_arduino.ino
+++ mar10a_arduino/mar10a_arduino.ino
@@ -1,9 +1,20 @@
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Serial.println("Buenos Dias Estrellitas C:");
+  pinMode(13,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (Serial.available()){
+    char letra = Serial.read()
+    Serial.print(letra);
+    if (letra == "H"){
+      digitalWrite(13,1);
+    }
+    else if (letra == "L"){
+      digitalWrite(13,0);  
+    }
+    }
 }
