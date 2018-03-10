92a43561e843f8c0be3fce3c3dedd837cc07b311
HootOwl17 <davidscout2010@hotmail.com>
2018-03-10 10:34:03 -0600

    Agregando otra configuracion

--- mar10a_arduino/mar10a_arduino.ino
+++ mar10a_arduino/mar10a_arduino.ino
@@ -1,3 +1,10 @@
+#define BuscarLed 1
+#define BuscarNumero 2
+
+int Led  = 0;
+int Dato = 0;
+int EstadoBusqueda = 0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -11,10 +18,27 @@ void loop() {
     char letra = Serial.read()
     Serial.print(letra);
     if (letra == "H"){
-      digitalWrite(13,1);
+      EstadoBusqueda = BuscarNumero;
     }
-    else if (letra == "L"){
-      digitalWrite(13,0);  
+    else if (letra == "D"){
+      EstadoBusqueda = BuscarNumero;  
+    }
+    else if(letra == "."){
+      analogWrite(Led,Dato);
+      Serial.print(Led);
+      Serial.print(" dato : ");
+      Serial.println(Dato);
+      Led  = 0;
+      Dato = 0;
+    }
+    else if(letra>='0' && letra <='9'){
+      int NumeroActual = int(letra - '0');
+      if (EstadoBusqueda == BuscarLed){
+        Led = Led * 10 + NumeroActual;
+      }
+      else if (EstadBusqueda == BuscarNumero){
+        Dato = Dato * 10 + NumeroActual;
+      }
     }
     }
 }
